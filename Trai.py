2fec9c3e3a1952600f058b7be2667265d88f7cb5
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-21 14:51:30 +0530

    Update Trai.py

--- Trai.py
+++ Trai.py
@@ -1,44 +1,44 @@
 import streamlit as st
 
-
 def navigation():
     try:
-        path = st.experimental_get_query_params()['p'][0]
+        query_params = st.experimental_get_query_params()
+        print("Query Params:", query_params)  # Print the query parameters
+        path = query_params.get('p', [''])[0]  # Use .get() to safely retrieve the value
     except Exception as e:
-        st.error('Please use the main app.')
+        st.error('Error occurred:', e)
         return None
     return path
 
+if __name__ == "__main__":
+    path = navigation()
 
-if navigation() == "home":
-    st.title('Home')
-    st.write('This is the home page.')
+    if path == "home":
+        st.title('Home')
+        st.write('This is the home page.')
 
-elif navigation() == "results":
-    st.title('Results List')
-    for item in range(25):
-        st.write(f'Results {item}')
+    elif path == "results":
+        st.title('Results List')
+        for item in range(25):
+            st.write(f'Results {item}')
 
-elif navigation() == "analysis":
-    st.title('Analysis')
-    x, y = st.number_input('Input X'), st.number_input('Input Y')
-    st.write('Result: ' + str(x+y))
+    elif path == "analysis":
+        st.title('Analysis')
+        x, y = st.number_input('Input X'), st.number_input('Input Y')
+        st.write('Result: ' + str(x + y))
 
-elif navigation() == "examples":
-    st.title('Examples Menu')
-    st.write('Select an example.')
+    elif path == "examples":
+        st.title('Examples Menu')
+        st.write('Select an example.')
 
+    elif path == "logs":
+        st.title('View all of the logs')
+        st.write('Here you may view all of the logs.')
 
-elif navigation() == "logs":
-    st.title('View all of the logs')
-    st.write('Here you may view all of the logs.')
+    elif path == "verify":
+        st.title('Data verification is started...')
+        st.write('Please stand by....')
 
-
-elif navigation() == "verify":
-    st.title('Data verification is started...')
-    st.write('Please stand by....')
-
-
-elif navigation() == "config":
-    st.title('Configuration of the app.')
-    st.write('Here you can configure the application')
+    elif path == "config":
+        st.title('Configuration of the app.')
+        st.write('Here you can configure the application')
