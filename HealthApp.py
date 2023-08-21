87a37acd53c74dab3efb6623a81c0c3ca9a40bd7
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-21 12:17:42 +0530

    Update HealthApp.py

--- HealthApp.py
+++ HealthApp.py
@@ -1,44 +1,64 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+EXAMPLE_NO = 1
 
 
-def navigation():
-    try:
-        path = st.experimental_get_query_params()['p'][0]
-    except Exception as e:
-        st.error('Please use the main app.')
-        return None
-    return path
+def streamlit_menu(example=1):
+    if example == 1:
+        # 1. as sidebar menu
+        with st.sidebar:
+            selected = option_menu(
+                menu_title="Main Menu",  # required
+                options=["Home", "Projects", "Contact"],  # required
+                icons=["house", "book", "envelope"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+            )
+        return selected
+
+    if example == 2:
+        # 2. horizontal menu w/o custom style
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+        )
+        return selected
+
+    if example == 3:
+        # 2. horizontal menu with custom style
+        selected = option_menu(
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "25px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "green"},
+            },
+        )
+        return selected
 
 
-if navigation() == "home":
-    st.title('Home')
-    st.write('This is the home page.')
+selected = streamlit_menu(example=EXAMPLE_NO)
 
-elif navigation() == "results":
-    st.title('Results List')
-    for item in range(25):
-        st.write(f'Results {item}')
-
-elif navigation() == "analysis":
-    st.title('Analysis')
-    x, y = st.number_input('Input X'), st.number_input('Input Y')
-    st.write('Result: ' + str(x+y))
-
-elif navigation() == "examples":
-    st.title('Examples Menu')
-    st.write('Select an example.')
-
-
-elif navigation() == "logs":
-    st.title('View all of the logs')
-    st.write('Here you may view all of the logs.')
-
-
-elif navigation() == "verify":
-    st.title('Data verification is started...')
-    st.write('Please stand by....')
-
-
-elif navigation() == "config":
-    st.title('Configuration of the app.')
-    st.write('Here you can configure the application')
+if selected == "Home":
+    st.title(f"You have selected {selected}")
+if selected == "Projects":
+    st.title(f"You have selected {selected}")
+if selected == "Contact":
+    st.title(f"You have selected {selected}")
