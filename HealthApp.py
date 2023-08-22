ba48264904cbab3039a4646f18c5a75bde4d6a61
Nikhil3137 <132592208+Nikhil3137@users.noreply.github.com>
2023-08-22 12:10:51 +0530

    Update HealthApp.py

--- HealthApp.py
+++ HealthApp.py
@@ -1,35 +1,61 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# ... (previous code)
+# 1=sidebar menu, 2=horizontal menu, 3=horizontal menu w/ custom menu
+EXAMPLE_NO = 1
 
-# Add a dropdown to select the example type
-example_type = st.selectbox("Select Example Type", ["Sidebar Menu", "Horizontal Menu", "Horizontal Menu with Custom Style"])
 
 def streamlit_menu(example=1):
-    if example == "Sidebar Menu":
+    if example == 1:
         # 1. as sidebar menu
         with st.sidebar:
             selected = option_menu(
-                # ... (previous code)
+                menu_title="Main Menu",  # required
+                options=["Home", "Projects", "Contact"],  # required
+                icons=["house", "book", "envelope"],  # optional
+                menu_icon="cast",  # optional
+                default_index=0,  # optional
+                orientation ="Horizontal",
             )
         return selected
 
-    if example == "Horizontal Menu":
+    if example == 2:
         # 2. horizontal menu w/o custom style
         selected = option_menu(
-            # ... (previous code)
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
         )
         return selected
 
-    if example == "Horizontal Menu with Custom Style":
-        # 3. horizontal menu with custom style
+    if example == 3:
+        # 2. horizontal menu with custom style
         selected = option_menu(
-            # ... (previous code)
+            menu_title=None,  # required
+            options=["Home", "Projects", "Contact"],  # required
+            icons=["house", "book", "envelope"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+            orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "25px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "green"},
+            },
         )
         return selected
 
-selected = streamlit_menu(example=example_type)
+
+selected = streamlit_menu(example=EXAMPLE_NO)
 
 if selected == "Home":
     st.title(f"You have selected {selected}")
